591ea7f46a198c0715edeaa08d5e8782c6664567
ghou8s <ghou8s@gmail.com>
2022-04-06 22:17:56 +0000

    vendor: show `AOSQP` version short [1/2]
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -1,4 +1,6 @@
 # Set all versions
+AOSQP_VERSION_NUMBER := v1.0
+
 AOSQP_DATE_YEAR := $(shell date -u +%Y)
 AOSQP_DATE_MONTH := $(shell date -u +%m)
 AOSQP_DATE_DAY := $(shell date -u +%d)
@@ -10,15 +12,16 @@ AOSQP_BUILD_DATE := $(AOSQP_DATE_YEAR)$(AOSQP_DATE_MONTH)$(AOSQP_DATE_DAY)-$(AOS
 TARGET_PRODUCT_SHORT := $(subst aosqp_,,$(AOSQP_BUILD))
 
 ifeq ($(WITH_GAPPS), true)
-AOSQP_VERSION := AOSQP_v1.0_$(AOSQP_BUILD)-GApps-$(AOSQP_BUILD_DATE)-BETA-QASSA
+AOSQP_VERSION := AOSQP_$(AOSQP_VERSION_NUMBER)_$(AOSQP_BUILD)-GApps-$(AOSQP_BUILD_DATE)-BETA-QASSA
 else
-AOSQP_VERSION := AOSQP_v1.0_$(AOSQP_BUILD)-Vanilla-$(AOSQP_BUILD_DATE)-BETA-QASSA
+AOSQP_VERSION := AOSQP_$(AOSQP_VERSION_NUMBER)_$(AOSQP_BUILD)-Vanilla-$(AOSQP_BUILD_DATE)-BETA-QASSA
 endif
 AOSQP_VERSION_PROP := Q
 
 AOSQP_PROPERTIES := \
     ro.aosqp.version=$(AOSQP_VERSION_PROP) \
     ro.aosqp.version.display=$(AOSQP_VERSION) \
+    ro.aosqp.version.number=$(AOSQP_VERSION_NUMBER) \
     ro.aosqp.build_date=$(AOSQP_BUILD_DATE) \
     ro.aosqp.build_date_utc=$(AOSQP_BUILD_DATE_UTC) \
     ro.aosqp.build_type=QASSA
