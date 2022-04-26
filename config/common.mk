Project-Xtended/vendor_xtended

vendor: split & update priv-app permissions

config/common.mk
@@ -75,7 +75,8 @@ PRODUCT_COPY_FILES += \
 # This is Xtended!
 PRODUCT_COPY_FILES += \
    vendor/xtended/config/permissions/privapp-permissions-google_prebuilt.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google_prebuilt.xml \
-   vendor/xtended/config/permissions/privapp-permissions-xtended.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-xtended.xml
+   vendor/xtended/config/permissions/privapp-permissions-xtended-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-xtended.xml \
+   vendor/xtended/config/permissions/privapp-permissions-xtended-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-xtended.xml
 
 # Hidden API whitelist
 PRODUCT_COPY_FILES += \

keepQASSA/vendor_qassa

Signed-off-by: rayrzy <vincentrayananda@gmail.com>
Change-Id: Icdc82c4d74bece3bd942238959cb3d3b0faf8519

config/common.mk
@@ -54,7 +54,9 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/aosqp/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
     vendor/aosqp/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lineagehw.xml \
-    vendor/aosqp/config/permissions/privapp-permissions-livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-livedisplay.xml
+    vendor/aosqp/config/permissions/privapp-permissions-livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-livedisplay.xml \
+    vendor/aosqp/config/permissions/privapp-permissions-aosqp-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosqp.xml \
+    vendor/aosqp/config/permissions/privapp-permissions-aosqp-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-aosqp.xml
 
 # Copy all aosqp init rc files
 $(foreach f,$(wildcard vendor/aosqp/prebuilt/common/etc/init/*.rc),\