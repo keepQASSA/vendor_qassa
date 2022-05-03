b83da267b674f387911f5a6f4334a348217820b8
ghou8s <ghou8s@gmail.com>
2022-05-03 19:14:45 +0000

    vendor: add `AOSQP` build date short
    * year, month, day. For https://github.com/AOSQP/packages_apps_Settings/commit/22488ff8c9fe2146b6ee7b29c7b5b7b0e53e6a20
    
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -7,6 +7,7 @@ AOSQP_DATE_DAY := $(shell date -u +%d)
 AOSQP_DATE_HOUR := $(shell date -u +%H)
 AOSQP_DATE_MINUTE := $(shell date -u +%M)
 AOSQP_BUILD_DATE_UTC := $(shell date -d '$(AOSQP_DATE_YEAR)-$(AOSQP_DATE_MONTH)-$(AOSQP_DATE_DAY) $(AOSQP_DATE_HOUR):$(AOSQP_DATE_MINUTE) UTC' +%s)
+AOSQP_BUILD_DATE_SHORT := $(AOSQP_DATE_YEAR)$(AOSQP_DATE_MONTH)$(AOSQP_DATE_DAY)
 AOSQP_BUILD_DATE := $(AOSQP_DATE_YEAR)$(AOSQP_DATE_MONTH)$(AOSQP_DATE_DAY)-$(AOSQP_DATE_HOUR)$(AOSQP_DATE_MINUTE)
 
 TARGET_PRODUCT_SHORT := $(subst aosqp_,,$(AOSQP_BUILD))
@@ -23,5 +24,6 @@ AOSQP_PROPERTIES := \
     ro.aosqp.version.display=$(AOSQP_VERSION) \
     ro.aosqp.version.number=$(AOSQP_VERSION_NUMBER) \
     ro.aosqp.build_date=$(AOSQP_BUILD_DATE) \
+    ro.aosqp.build_date_short=$(AOSQP_BUILD_DATE_SHORT) \
     ro.aosqp.build_date_utc=$(AOSQP_BUILD_DATE_UTC) \
     ro.aosqp.build_type=QASSA
