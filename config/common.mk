NusantaraProject-ROM/android_vendor_nusantara

charger: add Pixel charger animation - allow to opt-out with USE_PIXEL_CHARGING

config/common.mk
@@ -90,6 +90,9 @@ include vendor/nusantara/config/art.mk
 # Branding
 include vendor/nusantara/config/branding.mk
 
+# Charging animation
+include vendor/nusantara/config/charger.mk
+
 # Packages
 include vendor/nusantara/config/packages.mk
 

keepQASSA/vendor_qassa

config/common.mk
@@ -218,4 +218,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     StatusBarLyricExt
 
+# Charging animation
+include vendor/qassa/config/charger.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk