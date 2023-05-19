9283be6a97cde6def318944e3b78aadc7e9a0f79
ghou8s <ghou8s@gmail.com>
2023-05-19 18:16:51 +0000

    Sisu
    bertahan, berusaha melewati batas, pantang menyerah hingga berhasil.

--- config/branding.mk
+++ config/branding.mk
@@ -1,4 +1,5 @@
 # Set all versions
+QASSA_BUILD_CODENAME := Sisu
 QASSA_VERSION_NUMBER := v2.2
 
 QASSA_DATE_YEAR := $(shell date -u +%Y)
@@ -12,15 +13,16 @@ QASSA_BUILD_DATE := $(QASSA_DATE_YEAR)$(QASSA_DATE_MONTH)$(QASSA_DATE_DAY)-$(QAS
 TARGET_PRODUCT_SHORT := $(subst qassa_,,$(QASSA_BUILD))
 
 ifeq ($(WITH_GAPPS), true)
-QASSA_VERSION := qassa-$(QASSA_VERSION_NUMBER)-$(QASSA_BUILD)-$(QASSA_BUILD_DATE)-GApps
+QASSA_VERSION := qassa-$(QASSA_BUILD_CODENAME)-$(QASSA_VERSION_NUMBER)-$(QASSA_BUILD)-$(QASSA_BUILD_DATE)-GApps
 else
-QASSA_VERSION := qassa-$(QASSA_VERSION_NUMBER)-$(QASSA_BUILD)-$(QASSA_BUILD_DATE)-Vanilla
+QASSA_VERSION := qassa-$(QASSA_BUILD_CODENAME)-$(QASSA_VERSION_NUMBER)-$(QASSA_BUILD)-$(QASSA_BUILD_DATE)-Vanilla
 endif
 QASSA_VERSION_PROP := Q
 
 QASSA_PROPERTIES := \
     ro.qassa.version=$(QASSA_VERSION_PROP) \
     ro.qassa.version.display=$(QASSA_VERSION) \
+    ro.qassa.build.codename=$(QASSA_BUILD_CODENAME) \
     ro.qassa.version.number=$(QASSA_VERSION_NUMBER) \
     ro.qassa.build_date=$(QASSA_BUILD_DATE) \
     ro.qassa.build_date_utc=$(QASSA_BUILD_DATE_UTC)
