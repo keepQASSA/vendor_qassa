45c6190e3231d8f5c32948a2ad31bba957f16432
ghou8s <42911446+ghou8s@users.noreply.github.com>
2025-07-13 19:20:32 +0700

    vendor: add QASSA_BUILD_TYPE
    byNgantu (official)
    
    Signed-off-by: Lin Jian <vrayananda@yahoo.com>

--- config/branding.mk
+++ config/branding.mk
@@ -2,6 +2,9 @@
 QASSA_BUILD_CODENAME := Sisu
 QASSA_VERSION_NUMBER := v2.4.s
 
+# byNgantu (official)
+QASSA_BUILD_TYPE ?= COMMUNITY
+
 QASSA_DATE_YEAR := $(shell date -u +%Y)
 QASSA_DATE_MONTH := $(shell date -u +%m)
 QASSA_DATE_DAY := $(shell date -u +%d)
@@ -13,9 +16,9 @@ QASSA_BUILD_DATE := $(QASSA_DATE_YEAR)$(QASSA_DATE_MONTH)$(QASSA_DATE_DAY)-$(QAS
 TARGET_PRODUCT_SHORT := $(subst qassa_,,$(QASSA_BUILD))
 
 ifeq ($(WITH_GAPPS), true)
-QASSA_VERSION := qassa_$(QASSA_BUILD_CODENAME)-$(QASSA_VERSION_NUMBER)-$(QASSA_BUILD)-$(QASSA_BUILD_DATE)-GApps-signed
+QASSA_VERSION := qassa_$(QASSA_BUILD_CODENAME)-$(QASSA_VERSION_NUMBER)-$(QASSA_BUILD_TYPE)-$(QASSA_BUILD)-$(QASSA_BUILD_DATE)-GApps-signed
 else
-QASSA_VERSION := qassa_$(QASSA_BUILD_CODENAME)-$(QASSA_VERSION_NUMBER)-$(QASSA_BUILD)-$(QASSA_BUILD_DATE)-Vanilla-signed
+QASSA_VERSION := qassa_$(QASSA_BUILD_CODENAME)-$(QASSA_VERSION_NUMBER)-$(QASSA_BUILD_TYPE)-$(QASSA_BUILD)-$(QASSA_BUILD_DATE)-Vanilla-signed
 endif
 QASSA_VERSION_PROP := Q
 
@@ -23,6 +26,7 @@ QASSA_PROPERTIES := \
     ro.qassa.version=$(QASSA_VERSION_PROP) \
     ro.qassa.version.display=$(QASSA_VERSION) \
     ro.qassa.build.codename=$(QASSA_BUILD_CODENAME) \
+    ro.qassa.build.type=$(QASSA_BUILD_TYPE) \
     ro.qassa.version.number=$(QASSA_VERSION_NUMBER) \
     ro.qassa.build_date=$(QASSA_BUILD_DATE) \
     ro.qassa.build_date_utc=$(QASSA_BUILD_DATE_UTC)
